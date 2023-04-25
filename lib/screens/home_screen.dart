c22b603b9f5e96da258d0a28b3fcccfdb7fcebc2
altanod <altanod0627@gmail.com>
2023-04-25 17:14:38 +0800

    suggest words added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:converter/core/core.dart';
 import 'package:converter/core/custom_keyboard.dart';
 import 'package:converter/core/word_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:mongol/mongol.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -15,6 +14,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   TextEditingController controller = TextEditingController();
   List<String> sendHistory = [];
+  List<WordModel> suggest = [];
+  ScrollController scrollController = ScrollController();
 
   @override
   void initState() {
@@ -44,29 +45,58 @@ class _HomeScreenState extends State<HomeScreen> {
 
     Widget chatSection = Expanded(
       child: ListView(
+        controller: scrollController,
         children: [
           for (var item in sendHistory)
             Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 LimitedBox(
                   maxWidth: MediaQuery.of(context).size.width * .6,
-                  child: GestureDetector(
-                    onLongPress: () {
-                      Clipboard.setData(ClipboardData(text: item));
-                    },
+                  child: InkWell(
+                    onLongPress: () => showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => Dialog(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              const Text('Устгах уу?'),
+                              const SizedBox(height: 15),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: TextButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: const Text('Хаах'),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  Expanded(
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                        removeSearchString(item);
+                                      },
+                                      child: const Text('Устгах'),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
                     child: Container(
                       decoration: BoxDecoration(
                         color: color.primaryColor,
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(15),
-                          bottomLeft: Radius.circular(15),
-                          bottomRight: Radius.circular(15),
-                        ),
+                        borderRadius: const BorderRadius.only(topLeft: Radius.circular(15), bottomLeft: Radius.circular(15), bottomRight: Radius.circular(15)),
                       ),
                       margin: const EdgeInsets.only(bottom: 10),
                       padding: const EdgeInsets.all(15),
-                      child: RegExp(r'^[а-яөү]+$').hasMatch(item) ? Text(item, style: const TextStyle(color: Colors.white)) : MongolText(item, style: const TextStyle(color: Colors.white)),
+                      child: RegExp(r'^[а-яөү ]+$').hasMatch(item) ? Text(item, style: const TextStyle(color: Colors.white)) : MongolText(item, style: const TextStyle(color: Colors.white)),
                     ),
                   ),
                 ),
@@ -81,24 +111,70 @@ class _HomeScreenState extends State<HomeScreen> {
       app.isTraditionalKeyboard
           ? Container(
               margin: const EdgeInsets.only(right: 16),
-              child: MongolTextField(
-                controller: controller,
-                decoration: decoration,
-                style: TextStyle(fontSize: fontSize),
-                autofocus: true,
-                showCursor: true,
-                readOnly: true,
+              child: Row(
+                children: [
+                  MongolTextField(
+                    controller: controller,
+                    decoration: decoration,
+                    style: TextStyle(fontSize: fontSize),
+                    autofocus: true,
+                    showCursor: true,
+                    readOnly: true,
+                  ),
+                  const SizedBox(width: 4),
+                  LimitedBox(
+                    maxWidth: 30,
+                    child: ListView(
+                      children: [
+                        for (var item in suggest)
+                          InkWell(
+                            onTap: () => addText('${item.traditional} '),
+                            child: Container(
+                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(4), border: Border.all(color: Colors.black38)),
+                              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                              margin: EdgeInsets.only(bottom: suggest.last == item ? 0 : 8),
+                              child: Center(child: MongolText(item.traditional)),
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             )
           : Container(
               margin: const EdgeInsets.only(bottom: 16),
-              child: TextField(
-                controller: controller,
-                decoration: decoration,
-                style: TextStyle(fontSize: fontSize),
-                autofocus: true,
-                showCursor: true,
-                readOnly: true,
+              child: Column(
+                children: [
+                  const SizedBox(height: 16),
+                  LimitedBox(
+                    maxHeight: 40,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        for (var item in suggest)
+                          InkWell(
+                            onTap: () => addText('${item.krill} '),
+                            child: Container(
+                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(4), border: Border.all(color: Colors.black38)),
+                              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                              margin: EdgeInsets.only(right: suggest.last == item ? 0 : 8),
+                              child: Center(child: Text(item.krill)),
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    controller: controller,
+                    decoration: decoration,
+                    style: TextStyle(fontSize: fontSize),
+                    autofocus: true,
+                    showCursor: true,
+                    readOnly: true,
+                  ),
+                ],
               ),
             ),
       if (app.isTraditionalKeyboard) chatSection,
@@ -128,6 +204,7 @@ class _HomeScreenState extends State<HomeScreen> {
             onBackspace: backspace,
             onChangeKeyboard: () {
               controller.clear();
+              suggest = [];
               storage.setBool(SPKey.isTraditionalKeyboard.toString(), app.isTraditionalKeyboard);
               setState(() {});
             },
@@ -140,13 +217,38 @@ class _HomeScreenState extends State<HomeScreen> {
   void insertText(String myText) {
     controller.text = controller.text + myText;
     controller.selection = TextSelection.collapsed(offset: controller.text.length);
+    suggestSearch();
   }
 
   void backspace() {
     if (controller.text.isNotEmpty) {
       controller.text = controller.text.substring(0, controller.text.length - 1);
       controller.selection = TextSelection.collapsed(offset: controller.text.length);
+      suggestSearch();
     }
+  }
+
+  void suggestSearch() {
+    suggest = [];
+    String text = controller.text.split(' ').last;
+    if (text.isNotEmpty) {
+      suggest = app.words.where((element) => element.traditional.contains(text) || element.krill.contains(text)).toList();
+    }
+
+    setState(() {});
+  }
+
+  void addText(String text) {
+    String full = '';
+
+    for (var item in controller.text.split(' ')) {
+      if (item != controller.text.split(' ').last) {
+        full += '$item ';
+      }
+    }
+
+    controller.text = full;
+    insertText(text);
   }
 
   void search() {
@@ -163,7 +265,9 @@ class _HomeScreenState extends State<HomeScreen> {
       }
     }
     controller.clear();
-    saveSendString(string);
+    if (string.isNotEmpty) {
+      saveSendString(string);
+    }
   }
 
   void saveSendString(String string) {
@@ -202,5 +306,11 @@ class _HomeScreenState extends State<HomeScreen> {
     }
 
     setState(() {});
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (scrollController.hasClients) {
+        scrollController.jumpTo(scrollController.position.maxScrollExtent);
+      }
+    });
   }
 }
